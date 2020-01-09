09af39ee76c5d505009c47439a4e9dd2e7fd4371
Mallory Hightower <malhightower@gmail.com>
2020-01-09 17:02:55 -0600

    flask

---
+++ Flask/model.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import pickle
+
+data=pd.read_csv('/Users/malloryhightower/Desktop/Flask/dataset.csv')
+
+data=data.drop(['Unnamed: 5', 'Unnamed: 6','Index'], axis=1)
+
+X = data.iloc[:, :3]
+y = data.iloc[:, 3]
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+
+#Fitting model with trainig data
+regressor.fit(X, y)
+
+# Loading model to compare the results
+model = pickle.load(open('model.pkl','rb'))
+print(model.predict([[1,10,9]]))
+
