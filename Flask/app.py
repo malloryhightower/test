3e1c7cc6b5c48a5f0b6ca699e6bcfffb1b918474
malloryhightower <39606313+malloryhightower@users.noreply.github.com>
2020-03-05 17:21:39 -0600

    Update app.py

--- Flask/app.py
+++ Flask/app.py
@@ -17,7 +17,7 @@ def predict():
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     
-    if all(isinstance(item, int) for item in final_features) == True: # new code
+    if all(isinstance(item, int) for item in final_features) == False: # new code
         prediction = model.predict(final_features)
 
         output = round(prediction[0], 2)
