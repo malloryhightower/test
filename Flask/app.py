812090b887f5e230d35e425713e10e0985a0bdad
malloryhightower <39606313+malloryhightower@users.noreply.github.com>
2020-03-05 15:24:28 -0600

    Update app.py

--- Flask/app.py
+++ Flask/app.py
@@ -34,5 +34,4 @@ def predict_api():
     return jsonify(output)
 
 if __name__ == "__main__":
-    #app.run(host="0.0.0.0", port=443)
-    app.run(ssl_context='adhoc')
+    app.run(host="0.0.0.0", port=80)
