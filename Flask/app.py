6b06df3f49411632fff59b3d18d95062e62e6709
malloryhightower <39606313+malloryhightower@users.noreply.github.com>
2020-03-05 17:30:03 -0600

    Update app.py

--- Flask/app.py
+++ Flask/app.py
@@ -22,7 +22,7 @@ def predict():
 
         output = round(prediction[0], 2)
 
-        return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(final_features.dtype))
+        return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(type(final_features))
     
     return render_template('index.html', prediction_text='invalid input')
     
