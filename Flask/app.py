750cfe3dcee189d90dffa956b7edc8e5bfc9a23f
malloryhightower <39606313+malloryhightower@users.noreply.github.com>
2020-03-05 18:05:52 -0600

    Update app.py

--- Flask/app.py
+++ Flask/app.py
@@ -14,7 +14,7 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    if np.nan in request.form.values():
+    if np.isfinite in request.form.values() == False:
         return render_template('index.html', prediction_text='invalid input')
     else:
         int_features = [int(x) for x in request.form.values()]
